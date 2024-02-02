ea838a0430e2b8453955b2eff0fdd29d8311d1b0
Gabor Sajo <gabor@voyagerweb.ca>
2024-02-02 13:08:50 -0700

    Privacy Manifest

--- Package.swift
+++ Package.swift
@@ -20,7 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NVActivityIndicatorView",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+               .copy("Resources/PrivacyInfo.xcprivacy")
+            ]),
         .testTarget(
             name: "NVActivityIndicatorViewTests",
             dependencies: ["NVActivityIndicatorView"]),
